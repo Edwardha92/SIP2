d8d226258d0ad140605d48d423cbcbae9bb621b0
Edward <edward.alhanoon@gmail.com>
2018-04-24 01:13:52 +0200

    new sliding Windows

--- main.m
+++ main.m
@@ -42,31 +42,35 @@ function event = readEvents(path, fullpath)
     
     dataFiles = dir(strcat(fullpath, '*.mat'));
     %event.ecg = {};
-   
-    
+     
     new_akf=[];
     for fileIdx = 1:length(dataFiles)
         singleData = load(strcat(dataFiles(fileIdx).folder, '\', dataFiles(fileIdx).name));
         x=size(singleData.data.ecg);
-        y=fix(x/500)*500;
+        y=fix(x/140)*140;
         
-       for ecg_index=1:500:y(2)
-
-
-         ecg_fenster=singleData.data.ecg(ecg_index:ecg_index+500);
+       for ecg_index=1:140:y(2)
+         ecg_fenster=singleData.data.ecg(ecg_index:ecg_index+140);
          akf_fenster=xcorr(ecg_fenster,ecg_fenster);
          new_akf = [new_akf akf_fenster'];
- 
+         
        end
+%        plot(singleData.data.ecg);
+        plot(new_akf);
+%        imshow(new_akf);
+%        imagesc(new_akf);
         if strcmp(event.eventType,'APNAE')
 %            imshow(new_akf);
         end
-    end   
+    end
+    plot(new_akf);
+    imshow(new_akf);
     size_akf=size(new_akf);
     ii=fix(size_akf(2)/121)*121;
     for i =1:121:ii
-        sliding_window=new_akf(540:643,i:i+120);
+        sliding_window=new_akf(140:281,i:i+120);
         imshow(sliding_window);
+        imagesc(sliding_window);
     end
 end   
     
