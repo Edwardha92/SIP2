ed43bc2ca08fe60e2bdf028f1ce4f928a8e45d8e
Stanislav Uschakow <stanislav.uschakow@gmail.com>
2018-04-25 18:15:58 +0200

    Introducing function get_ecg which needs the root path to the data files and returns two vectors for no_event_ecg and ap_event_ecg (apnoe) ecg data from all files it could find.

---
+++ get_ecg.m
@@ -0,0 +1,38 @@
+function [no_events_ecg, ap_events_ecg] = get_ecg(root_path)
+    data_path = dir(root_path);
+    data_content = [data_path(:).isdir];
+    folders = {data_path(data_content).name}';
+    folders(ismember(folders,{'.', '..'})) = [];
+    
+    no_events_ecg = [];
+    ap_events_ecg = [];
+    
+    
+    for folder_idx = 1:length(folders)
+       tmp_path = strcat(root_path, '\', folders{folder_idx});
+       events_path = dir(tmp_path);
+       event_content = [events_path(:).isdir];
+       single_event_path = {events_path(event_content).name};
+       single_event_path(ismember(single_event_path,{'.', '..'})) = [];
+       events_full_path = strcat(tmp_path, '\', single_event_path);
+       sprintf('file %d of %d', folder_idx, length(folders))
+       for event_type_idx = 1:length(events_full_path)
+           event_files = dir(strcat(events_full_path{event_type_idx}, '\*.mat'));
+           tmp_events = [];
+           
+           for file_idx = 1:length(event_files)
+               file = load(strcat(event_files(file_idx).folder,'\', event_files(file_idx).name));
+               data = file.data.ecg;
+               tmp_events = [tmp_events; data(1:6513)];
+           end
+           
+           if strfind(events_full_path{event_type_idx}, 'NO_EVENT')
+               no_events_ecg = [no_events_ecg; tmp_events];
+           else
+               ap_events_ecg = [ap_events_ecg; tmp_events];
+           end
+       end
+    end
+    
+    
+end
--- main.m
+++ main.m
@@ -1,74 +1,87 @@
 clc;
 clear;
 
-rootPath = 'C:\Users\Edwar\Desktop\3.Semester\SIP2\Datenbank\best_events\data';
-%rootPath = 'bestData/data';
+root_path = 'data';
+
+[no_event, ap_event] = get_ecg(root_path);
+
+1+3
 
 
-dataDir = dir(rootPath);%%List folder contents
-dataDirVect = [dataDir(:).isdir]; %# returns logical vector
-patientFolders = {dataDir(dataDirVect).name}';
-patientFolders(ismember(patientFolders,{'.','..'})) = [];%%Delet the empty felds
 
-global event;
-event = struct('eventType', '', 'ecg', [], 'path', '');
-events = {};
 
-for patIdx = 1:length(patientFolders)
-    patientSubFolders = dir(strcat(rootPath, '\' ,patientFolders{patIdx}));%to see the contents of the PatientFolder
-    subidx = [patientSubFolders(:).isdir];% returns logical vector
-    patFolderNames = {patientSubFolders(subidx).name}';
-    patFolderPath = {patientSubFolders(subidx).folder}';
-    patFolderNames(ismember(patFolderNames,{'.','..'})) = [];
-    
-    for sub = 1:length(patFolderNames)
-       eventFolderName = patFolderNames{sub};
-       fullPath = strcat(patFolderPath{1},'\',eventFolderName,'\');%Concatenate strings horizontally
-       events{end+1} = readEvents(eventFolderName, fullPath); 
-    end
-end
 
-function event = readEvents(path, fullpath)
-    
-    if (nargin ~= 1) %Number of function input arguments
-        event.path = fullpath;
-    end
-    if (strcmp(path,'NO_EVENT'))%Compare strings
-        event.eventType = 'NO';
-    end
-    if (strcmp(path,'Obstructive Apnea'))
-        event.eventType = 'APNAE';
-    end
-    
-    dataFiles = dir(strcat(fullpath, '*.mat'));
-    %event.ecg = {};
-     
-    new_akf=[];
-    for fileIdx = 1:length(dataFiles)
-        singleData = load(strcat(dataFiles(fileIdx).folder, '\', dataFiles(fileIdx).name));
-        x=size(singleData.data.ecg);
-        y=fix(x/141)*141;
-        
-       for ecg_index=1:141:y(2)
-         ecg_fenster=singleData.data.ecg(ecg_index:ecg_index+141);
-         akf_fenster=xcorr(ecg_fenster,ecg_fenster);
-         new_akf = [new_akf akf_fenster'];
-         
-       end
-%        plot(singleData.data.ecg);
-%        plot(new_akf);
-        if strcmp(event.eventType,'APNAE')
-%            imshow(new_akf);
-        end
-    end
-%    plot(new_akf);
-%    imshow(new_akf);
-    size_akf=size(new_akf);
-    ii=fix(size_akf(2)/121)*121;
-    for i =1:121:ii
-        sliding_window=new_akf(179:282,i:i+120);
-        imshow(sliding_window);
-        imagesc(sliding_window);
-    end
-end   
-    
+
+
+% 
+% % rootPath = 'C:\Users\Edwar\Desktop\3.Semester\SIP2\Datenbank\best_events\data';
+% rootPath = 'data';
+% 
+% 
+% dataDir = dir(rootPath);%%List folder contents
+% dataDirVect = [dataDir(:).isdir]; %# returns logical vector
+% patientFolders = {dataDir(dataDirVect).name}';
+% patientFolders(ismember(patientFolders,{'.','..'})) = [];%%Delet the empty felds
+% 
+% global event;
+% event = struct('eventType', '', 'ecg', [], 'path', '');
+% events = {};
+% 
+% for patIdx = 1:length(patientFolders)
+%     patientSubFolders = dir(strcat(rootPath, '\' ,patientFolders{patIdx}));%to see the contents of the PatientFolder
+%     subidx = [patientSubFolders(:).isdir];% returns logical vector
+%     patFolderNames = {patientSubFolders(subidx).name}';
+%     patFolderPath = {patientSubFolders(subidx).folder}';
+%     patFolderNames(ismember(patFolderNames,{'.','..'})) = [];
+%     
+%     for sub = 1:length(patFolderNames)
+%        eventFolderName = patFolderNames{sub};
+%        fullPath = strcat(patFolderPath{1},'\',eventFolderName,'\');%Concatenate strings horizontally
+%        events{end+1} = readEvents(eventFolderName, fullPath); 
+%     end
+% end
+% 
+% function event = readEvents(path, fullpath)
+%     
+%     if (nargin ~= 1) %Number of function input arguments
+%         event.path = fullpath;
+%     end
+%     if (strcmp(path,'NO_EVENT'))%Compare strings
+%         event.eventType = 'NO';
+%     end
+%     if (strcmp(path,'Obstructive Apnea'))
+%         event.eventType = 'APNAE';
+%     end
+%     
+%     dataFiles = dir(strcat(fullpath, '*.mat'));
+%     %event.ecg = {};
+%      
+%     new_akf=[];
+%     for fileIdx = 1:length(dataFiles)
+%         singleData = load(strcat(dataFiles(fileIdx).folder, '\', dataFiles(fileIdx).name));
+%         x=size(singleData.data.ecg);
+%         y=fix(x/141)*141;
+%         
+%        for ecg_index=1:141:y(2)
+%          ecg_fenster=singleData.data.ecg(ecg_index:ecg_index+141);
+%          akf_fenster=xcorr(ecg_fenster,ecg_fenster);
+%          new_akf = [new_akf akf_fenster'];
+%          
+%        end
+% %        plot(singleData.data.ecg);
+% %        plot(new_akf);
+%         if strcmp(event.eventType,'APNAE')
+% %            imshow(new_akf);
+%         end
+%     end
+% %    plot(new_akf);
+% %    imshow(new_akf);
+%     size_akf=size(new_akf);
+%     ii=fix(size_akf(2)/121)*121;
+%     for i =1:121:ii
+%         sliding_window=new_akf(179:282,i:i+120);
+% %         imshow(sliding_window);
+%         imagesc(sliding_window);
+%     end
+% end   
+%     
