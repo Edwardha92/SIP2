ff79605f7d20aaec14b21a16875c5695f14fd196
edward <edward.alhanoon@gmail.com>
2018-04-28 08:31:16 +0000

    Update fenster.m

--- fenster.m
+++ fenster.m
@@ -1,8 +1,8 @@
-function akf_fenster=fenster(new_akf)
-    size_akf=size(new_akf);
+function akf_fenster=fenster(akf_list)
+    size_akf=size(akf_list);
     ii=fix(size_akf(1)/121)*121;
     for i =1:121:ii
-        sliding_window=new_akf(ii/2+39:ii/2+142,i:i+120);
+        akf_fenster=new_akf(ii/2+39:ii/2+142,i:i+120);
         imshow(sliding_window);
         imagesc(sliding_window);
         
