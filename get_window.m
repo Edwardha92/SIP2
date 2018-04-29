07e5c68e63309ecdc55987c438f759c25f2a0b8a
Stanislav Uschakow <stanislav.uschakow@gmail.com>
2018-04-29 20:03:53 +0200

    Extracting max values from akf singal is done.

--- get_window.m
+++ get_window.m
@@ -1,10 +1,57 @@
 function akf_window = get_window(akf_list)
     size_akf=size(akf_list);
+    akf_window = [];
     
     for i =1:121:size_akf(2) - 121
-        
-        akf_window = [akf_window akf_list(size_akf(1)/2 + 39:size_akf(1) / 2 + 142, i:i + 120)];
+        tmp = akf_list(size_akf(1)/2 + 39:size_akf(1) / 2 + 142, i:i + 120);
+        akf_window = [akf_window tmp];
         %imshow(akf_fenster);hold on;
-        imagesc(akf_fenster);
+        imagesc(akf_window);
+        
+        process(tmp);
+        
     end
 end 
+
+function process(akf_window)
+    max_values = [];
+    max_idx = [];
+    for i = 1:121
+        mx = max(akf_window(:,i));
+        max_idx = [max_idx find(akf_window(:,i) == mx)];
+        max_values = [max_values mx];
+    end
+        
+    mmax = max(max_values);
+    nm = zeros(size(akf_window));
+    for i = 1:length(max_values)
+        max_values(i) = max_values(i) / mmax;
+        nm(max_idx(i), i) = 10;
+    end
+    
+    b = get_filter_coeff();
+    
+    new_max = filter(b, [1], max_values);
+    subplot(2,1,1);plot(max_values);
+    subplot(2,1,2);plot(new_max);
+    
+    sampled_data = [];
+    len = fix(length(new_max)/13);
+    for i = 1:len:length(new_max) - len
+        sampled_data = [sampled_data new_max(i)];
+    end
+end
+
+function coeff = get_filter_coeff()
+    fs    = 20;     % Sampling Frequency
+
+    N     = 10;     % Order
+    Fpass = 5;      % Passband Frequency
+    Fstop = 10;     % Stopband Frequency
+    Wpass = 10;     % Passband Weight
+    Wstop = 10;     % Stopband Weight
+    dens  = 20;     % Density Factor
+
+    coeff  = firpm(N, [0 Fpass Fstop Fs/2]/(Fs/2), [1 1 0 0], [Wpass Wstop], {dens});
+    Hd = dfilt.dffir(b);
+end
--- new_main.m
+++ new_main.m
@@ -18,52 +18,9 @@ for event_idx = 1:10% length(ap_event(:,1))
 %     subplot(2,1,1); plot(ecg); hold on; 
 %     subplot(2,1,2); plot(akf); hold on;
 end
-imshow(akf);
 
-akf_fenster=fenster(akf);
+% imshow(akf);
 
+akf_window = get_window(akf);
+process_akf(akf_window);
 
-% dataDir = dir(rootPath);%%List folder contents
-% dataDirVect = [dataDir(:).isdir]; %# returns logical vector
-% patientFolders = {dataDir(dataDirVect).name}';
-% patientFolders(ismember(patientFolders,{'.','..'})) = [];%%Delet the empty felds
-% 
-% global event;
-% event = struct('eventType', '', 'ecg', [], 'path', '');
-% events = {};
-% 
-% for patIdx = 1:length(patientFolders)
-%     patientSubFolders = dir(strcat(rootPath, '\' ,patientFolders{patIdx}));%to see the contents of the PatientFolder
-%     subidx = [patientSubFolders(:).isdir];% returns logical vector
-%     patFolderNames = {patientSubFolders(subidx).name}';
-%     patFolderPath = {patientSubFolders(subidx).folder}';
-%     patFolderNames(ismember(patFolderNames,{'.','..'})) = [];
-%     
-%     for sub = 1:length(patFolderNames)
-%        eventFolderName = patFolderNames{sub};
-%        fullPath = strcat(patFolderPath{1},'\',eventFolderName,'\');%Concatenate strings horizontally
-%        events{end+1} = readEvents(eventFolderName, fullPath); 
-%     end
-% end
-% 
-% function event = readEvents(path, fullpath)
-%     
-%     if (nargin ~= 1) %Number of function input arguments
-%         event.path = fullpath;
-%     end
-%     if (strcmp(path,'NO_EVENT'))%Compare strings
-%         event.eventType = 'NO';
-%     end
-%     if (strcmp(path,'Obstructive Apnea'))
-%         event.eventType = 'APNAE';
-%     end
-%     
-%     dataFiles = dir(strcat(fullpath, '*.mat'));
-%     
-%     for fileIdx = 1:length(dataFiles)
-%         singleData = load(strcat(dataFiles(fileIdx).folder, '\', dataFiles(fileIdx).name));
-%         calculate_akf(singleData.data.ecg);
-%     end
-%     
-% end   
-%     
