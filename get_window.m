2220da6130d71ce456ea4f4a5d0547214ed248df
Stanislav Uschakow <stanislav.uschakow@gmail.com>
2018-04-29 19:03:22 +0200

    Renamed fenster to get_window.

--- get_window.m
+++ get_window.m
@@ -1,9 +1,9 @@
-function akf_fenster=fenster(akf_list )
+function akf_window = get_window(akf_list)
     size_akf=size(akf_list);
     
     for i =1:121:size_akf(2) - 121
         
-        akf_fenster=akf_list(size_akf(1)/2 + 39:size_akf(1) / 2 + 142, i:i + 120);
+        akf_window = [akf_window akf_list(size_akf(1)/2 + 39:size_akf(1) / 2 + 142, i:i + 120)];
         %imshow(akf_fenster);hold on;
         imagesc(akf_fenster);
     end
