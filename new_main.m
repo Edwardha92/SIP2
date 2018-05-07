6fd866fd79a3ebfe349b6a9df048c9ca83bc42b5
Stanislav Uschakow <stanislav.uschakow@gmail.com>
2018-05-07 17:23:19 +0200

    Merged version from SIP2 meeting in the morning and the version I forgot to commit from the weekend before.

--- new_main.m
+++ new_main.m
@@ -3,10 +3,10 @@ clear;
 % figure(1);
 %root_path = 'C:\Users\Edwar\Desktop\3.Semester\SIP2\Datenbank\best_events\data';
 % root_path = 'data';
-root_path = 'C:\Users\bloodsurfer\Documents\1dev\SIP2\bestData\data';
+root_path = 'D:/1dev/SIP2/data';
 
 [no_event, ap_event] = get_ecg(root_path);
-akf = [];
+
 ap = [];
 no = [];
 
@@ -16,15 +16,21 @@ no_event = no_event(1:len,:);
 
 
 for event_idx = 1:length(ap_event(:,1))
-    ap_ecg = ap_event(event_idx,:);    
-    ap_akf = calculate_akf(ap_ecg);    
+    ap_ecg = ap_event(event_idx,:);
+    ap_akf = calculate_akf(ap_ecg);
     ap = [ap ap_akf];
+    if mod(event_idx, 100) == 0
+       sprintf('Apnoe event processed: %f', (100 / len) * event_idx)
+    end
 end
 
 for event_idx = 1:length(no_event(:,1))
     no_ecg = no_event(event_idx,:);
     no_akf = calculate_akf(no_ecg);
     no = [no no_akf];
+    if mod(event_idx, 100) == 0
+       sprintf('No event processed: %f', (100 / len) * event_idx)
+    end
 end
 
 figure(1);imagesc(ap);
@@ -46,7 +52,13 @@ sorted_target = [ap_target no_target];
 shuffle_idx = randperm(size(sorted_data,2));
 
 training_data = sorted_data(:,shuffle_idx);
-target = sorted_target(:, shuffle_idx);
-ann(training_data, target);
+target = sorted_target(:,shuffle_idx);
 
+training_data = training_data/norm(training_data);
+
+nnstart;
+
+% ann(training_data, target);
+
+%process_akf(akf_window);
 
